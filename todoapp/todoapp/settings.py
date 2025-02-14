6ac4b81e6ae5be7482a554cb71a5a7384a21e2b0
Suryan2820 <suryan2820@gmail.com>
2025-02-15 01:02:27 +0530

    Add ALLOWED_HOSTS

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9heiil^sfd0(9qmj7%m%oi7f0%l9fr1exr0^2pr=l#qpiu8u29
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://suryan2820.pythonanywhere.com/"]
 LOGIN_URL = '/login/'
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
